c8692f4ecec24d7fd0cf74a94265f2bfadadbb9a
Lukas Buchli <lbu01@buchli-bigger.ch>
2019-08-03 16:54:26 +0200

    Fix level transitions

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -4,6 +4,9 @@ export (Array, PackedScene) var levels
 
 var current_level = 0
 var current_level_instance = null
+
+var _next_scene_mutex = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_level_instance = levels[current_level].instance()
@@ -18,7 +21,7 @@ func _on_Player_screen_entered():
 	_reset_player()
 
 func _on_Player_screen_exited():
-	_reset_scene()
+	call_deferred("_reset_scene")
 
 func _on_Player_asleep():
 	_reset_scene()
@@ -34,10 +37,15 @@ func _reset_scene():
 	_reset_player()
 
 func _next_scene():
+	_next_scene_mutex = true
 	$FadeOut.play("fade")
 	yield($FadeOut, "animation_finished")
+	remove_child(current_level_instance)
 	current_level += 1
-	get_tree().change_scene_to(levels[current_level])
+	current_level_instance = levels[current_level].instance()
+	add_child(current_level_instance)
+	_reset_player()
+	_next_scene_mutex = false
 
 func _on_Player_energy_changed(energy_level):
 	var tilemap = current_level_instance.get_node("TileMap")
@@ -46,8 +54,9 @@ func _on_Player_energy_changed(energy_level):
 	if tilemap.get_cellv(ppos) == 1: # check for coffee
 		 tilemap.set_cellv(ppos, -1)
 		 $Player.set("current_energy", $Player.ENERGY)
-	elif tilemap.get_cellv(ppos) == 0: # check for bed
-		_next_scene()
+	elif tilemap.get_cellv(ppos) == 2: # check for bed
+		if not _next_scene_mutex:
+			_next_scene()
 	
 	var darkness =  ($Player.ENERGY - energy_level)/($Player.ENERGY*2)
 	set("modulate", Color(1-darkness, 1-darkness, 1-darkness))
