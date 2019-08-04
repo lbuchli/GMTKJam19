59450369193dfbdc7585082e54a7994457fd328b
Lukas Buchli <lbu01@buchli-bigger.ch>
2019-08-04 10:51:06 +0200

    Reverse player input carry

--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -22,9 +22,7 @@ func _on_Player_asleep():
 	_reset_scene()
 
 func _reset_player():
-	var last_input = $Player.current_input # carry input
 	$Player._init()
-	$Player.current_input = last_input
 	$Player.position = $StartPosition.position
 
 func _reset_scene():
